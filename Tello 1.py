e97141bba05d0ebff9fab8461b567c6363b88d4a
KarenH <167257720+KarenHzj@users.noreply.github.com>
2024-04-18 17:33:24 -0600

    Add files via upload

---
+++ Tello 1.py
@@ -0,0 +1,23 @@
+import djitellopy
+from djitellopy import Tello
+
+dron = Tello()
+dron.connect()
+
+##detectar objeto
+
+
+##Revisar bateria
+x = dron.get_battery()
+print('XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX')
+print('Battery: ', x)
+
+##Despegar
+dron.takeoff()
+
+dron.move_up(50)
+dron.rotate_clockwise(360)
+dron.move_forward(50)
+
+##Aterrizar
+dron.land()
